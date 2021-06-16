84324d6582b7224a3e263fe9d049b5d9bed68f1b
OM Miles <miles@ommiles.com>
2021-06-16 10:35:43 -0700

    lab complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  resources :artists
-  resources :songs
+  # resources :artists
+  # resources :songs
+
+  root 'songs#index'
+  resources :artists, only: [:index, :show] do
+    # nested resource for songs
+    resources :songs, only: [:index, :show]
+  end
+
+  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
 end
